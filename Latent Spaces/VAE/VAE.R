6dfb8a2321dd0a98ba0acdc4b397e5d25d024e89
Rafael S. de Souza <rafael.2706@gmail.com>
2025-07-07 20:05:27 -0300

    ML vs RF

--- Latent Spaces/VAE/VAE.R
+++ Latent Spaces/VAE/VAE.R
@@ -1,68 +1,64 @@
-#––– 0) Libraries
-library(keras)
+library(keras3)
 library(RDRToolbox)
 library(ggplot2)
 library(viridis)
 
-#––– 1) Generate Swiss Roll
+# 1. Data
 set.seed(42)
-swiss <- SwissRoll(2000, plot = FALSE)
+swiss <- SwissRoll(2000)
 x_raw <- swiss
-t_est <- sqrt(x_raw[,1]^2 + x_raw[,3]^2)   # "true" roll coordinate
+t_est <- sqrt(x_raw[,1]^2 + x_raw[,3]^2)
 x_scaled <- scale(x_raw)
+input_dim <- ncol(x_scaled)
+latent_dim <- 2
 
-#––– 2) Sampling function
+# 2. Sampling function for Lambda
 sampling <- function(args) {
   z_mean <- args[[1]]
   z_log_var <- args[[2]]
-  epsilon <- k_random_normal(shape = k_shape(z_mean), mean = 0., stddev = 1)
+  epsilon <- k_random_normal(shape = k_shape(z_mean), mean = 0, stddev = 1)
   z_mean + k_exp(0.5 * z_log_var) * epsilon
 }
 
-#––– 3) Encoder
-latent_dim <- 2
-input_dim <- ncol(x_scaled)
-
+# 3. Encoder
 inputs <- layer_input(shape = input_dim, name = "encoder_input")
-
 h <- inputs %>%
   layer_dense(128, activation = "relu") %>%
   layer_dense(64,  activation = "relu")
-
 z_mean <- h %>% layer_dense(latent_dim, name = "z_mean")
 z_log_var <- h %>% layer_dense(latent_dim, name = "z_log_var")
-
 z <- list(z_mean, z_log_var) %>%
   layer_lambda(sampling, name = "z")
+encoder <- keras_model(inputs, list(z_mean, z_log_var, z), name = "encoder")
 
-encoder <- keras_model(inputs, list(z_mean, z_log_var, z))
-
-#––– 4) Decoder
-decoder_input <- layer_input(shape = latent_dim, name = "decoder_input")
-
-decoder_output <- decoder_input %>%
+# 4. Decoder
+latent_inputs <- layer_input(shape = latent_dim, name = "z_sampling")
+decoder_h <- latent_inputs %>%
   layer_dense(64,  activation = "relu") %>%
-  layer_dense(128, activation = "relu") %>%
-  layer_dense(input_dim, activation = "linear")
+  layer_dense(128, activation = "relu")
+outputs <- decoder_h %>% layer_dense(input_dim, activation = "linear")
+decoder <- keras_model(latent_inputs, outputs, name = "decoder")
 
-decoder <- keras_model(decoder_input, decoder_output)
-
-#––– 5) VAE Model
-z_vals <- encoder(inputs)
-reconstruction <- decoder(z_vals[[3]])
+# 5. VAE Model (symbolic tensors)
+z_mean_out <- encoder(inputs)[[1]]
+z_log_var_out <- encoder(inputs)[[2]]
+z_sampled <- encoder(inputs)[[3]]
+reconstruction <- decoder(z_sampled)
 
 vae <- keras_model(inputs, reconstruction)
 
-#––– 6) Custom loss function
-vae_loss <- function(x, x_decoded) {
-  rec <- k_sum(k_square(x - x_decoded), axis = -1L)
-  kl  <- -0.5 * k_sum(1 + z_vals[[2]] - k_square(z_vals[[1]]) - k_exp(z_vals[[2]]), axis = -1L)
-  k_mean(rec + kl)
-}
+# 6. Register KL loss to the model using add_loss
+kl_loss <- -0.5 * k_mean(
+  1 + z_log_var_out - k_square(z_mean_out) - k_exp(z_log_var_out),
+  axis = -1L
+)
+vae$add_loss(kl_loss)
 
-vae %>% compile(optimizer = "adam", loss = vae_loss)
+vae %>% compile(
+  optimizer = optimizer_adam(),
+  loss      = "mse"   # Just the reconstruction loss; KL added with add_loss
+)
 
-#––– 7) Train
 vae %>% fit(
   x_scaled, x_scaled,
   epochs = 200,
@@ -71,19 +67,17 @@ vae %>% fit(
   verbose = 0
 )
 
-#––– 8) Get latent space (z_mean)
-encoded <- predict(encoder, x_scaled)
-z_mean_vals <- encoded[[1]]
-
+# 7. Get latent codes
+z_mean_mat <- predict(encoder, x_scaled)[[1]]
 vae_df <- data.frame(
-  Lat1 = z_mean_vals[,1],
-  Lat2 = z_mean_vals[,2],
+  Lat1 = z_mean_mat[,1],
+  Lat2 = z_mean_mat[,2],
   roll = t_est
 )
 
-#––– 9) Plot
+# 8. Plot latent space
 ggplot(vae_df, aes(Lat1, Lat2, color = roll)) +
-  geom_point(size = 1.5) +
+  geom_point(size = 1.3) +
   scale_color_viridis_c() +
   theme_minimal() +
-  ggtitle("VAE Latent Space — Swiss Roll")
+  ggtitle("VAE Latent Space (keras3, R-native)")
